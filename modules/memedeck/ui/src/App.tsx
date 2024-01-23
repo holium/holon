14555b97284d919ce702c3d265a33415f5b767d3
gdbroman <99gustaf@gmail.com>
2024-01-23 21:26:48 +0100

    Init tailwind

--- modules/memedeck/ui/src/App.tsx
+++ modules/memedeck/ui/src/App.tsx
@@ -1,5 +1,4 @@
 import { useState, useEffect } from "react";
-import "./App.css";
 
 const BASE_URL = import.meta.env.BASE_URL;
 if (window.our) window.our.process = BASE_URL?.replace("/", "");
@@ -18,20 +17,24 @@ function App() {
   }, []);
 
   return (
-    <div style={{ width: "100%" }}>
-      <div style={{ position: "absolute", top: 4, left: 8 }}>
-        ID: <strong>{window.our?.node}</strong>
+    <div>
+      <header className="flex items-center px-4 h-14 w-full">
+        <p>
+          ID: <strong>{window.our?.node}</strong>
+        </p>
+      </header>
+      <div className="px-4">
+        <h1>Memedeck</h1>
+        {!nodeConnected && (
+          <div className="node-not-connected">
+            <h2 style={{ color: "red" }}>Node not connected</h2>
+            <h4>
+              You need to start a node at {PROXY_TARGET} before you can use this UI
+              in development.
+            </h4>
+          </div>
+        )}
       </div>
-      <h1>Memedeck</h1>
-      {!nodeConnected && (
-        <div className="node-not-connected">
-          <h2 style={{ color: "red" }}>Node not connected</h2>
-          <h4>
-            You need to start a node at {PROXY_TARGET} before you can use this UI
-            in development.
-          </h4>
-        </div>
-      )}
     </div>
   );
 }
