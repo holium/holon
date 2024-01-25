21a1975cc0575903ed8030dacdcc1caeb1eed84a
gdbroman <99gustaf@gmail.com>
2024-01-25 10:56:32 +0100

    cleanup

---
+++ modules/memedeck/ui/tailwind.config.ts
@@ -0,0 +1,17 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        black: {
+          200: "rgba(0, 0, 0, 0.32)",
+          400: "#191919",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
+export default config;
