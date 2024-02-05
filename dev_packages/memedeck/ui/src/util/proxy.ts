8bbd6b1011d94a420d9e3cc69bb977155fb5d3b9
DrunkPlato <drunkplato@DrunkPlatos-M3.local>
2024-02-05 06:10:24 -0600

    set submodule to graphdb branch

--- dev_packages/memedeck/ui/src/util/proxy.ts
+++
@@ -1,5 +0,0 @@
-export const BASE_URL = import.meta.env.BASE_URL;
-
-if (window.our) window.our.process = BASE_URL?.replace("/", "");
-
-export const PROXY_TARGET = `${import.meta.env.VITE_NODE_URL || "http://localhost:8080"}${BASE_URL}`;
